2a5f93952b8ea0625df8ed1b73546e0e8b8c71fa
Envoudige <56773097+mentols@users.noreply.github.com>
2023-04-08 09:57:42 +0300

    edjust k8s nginx configs and .env

--- core/producer.py
+++ core/producer.py
@@ -6,7 +6,7 @@ from app.config import Config
 
 # todo: add login
 async def send_one(message: dict):
-    producer = AIOKafkaProducer(bootstrap_servers='kafka:9092')
+    producer = AIOKafkaProducer(bootstrap_servers=Config.Server)
     await producer.start()
     try:
         await producer.send_and_wait(Config.Topic, json.dumps(message).encode('utf-8'))
